f1c6d30b2365d937dd6d4437b349e62c474b210c
Yngve Magnussen <74648054+yngvemag@users.noreply.github.com>
2023-02-01 12:31:26 +0100

    Add project files.

---
+++ Console App Introduksjon/Gokstad/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Console_App_Introduksjon.Gokstad
+{
+
+    /*
+      * Person
+              - navn (string)
+              - fødselsår (int)
+              - ???
+              - ???
+              - Alder ??
+              - 
+     */
+    public class Person
+    {
+        // konstruktør
+        public Person() { }
+
+        public Person(string name, int birthYear)
+        {
+            // this -> "dette objektet"
+            Name = name;
+            BirthYear = birthYear;
+        }
+
+        // Properties
+        public string? Name { get; set; }
+
+        public int BirthYear { get; set; }
+    }
+
+}
---
+++ Console App Introduksjon/Program.cs
@@ -0,0 +1,42 @@
+﻿using Console_App_Introduksjon.Gokstad;
+
+internal class Program
+{   
+    /*
+        - Lage skriver ut navn og fødselssår på personer
+        
+        Lage en beskrivelse av dette!
+        'class' -> oppskrift på noe!!
+        
+        eks: bake kake:
+            oppskrift : 'class'
+            prosess lage kake
+                'new' !!!
+                    konstruktører 
+                    konstruktører (med nonstop)
+            når kaka er ferdig og du kan spise: 'object'
+
+        * Person
+              - navn (Field, Property!!)
+              - fødselsår (Property)
+     
+     */
+
+    private static void Main(string[] args)
+    {
+
+        // Person()
+        Person yngve = new Person();
+        yngve.Name = "Yngve Magnussen";
+        yngve.BirthYear = 1973;
+
+
+        // Person(string name, int birthYear)
+        Person ola = new ("Ola Normann", 2000);
+
+
+        Console.WriteLine($"Navnet på person er {yngve.Name} og er født i år {yngve.BirthYear}");
+        Console.WriteLine($"Navnet på person er {ola.Name} og er født i år {ola.BirthYear}");
+
+    }
+}
