ba23a6511f8432834ff9609090e9f656732b6409
Yngve Magnussen <74648054+yngvemag@users.noreply.github.com>
2023-02-08 12:43:51 +0100

    after session 2

--- Console App Introduksjon/Gokstad/Person.cs
+++ Console App Introduksjon/Gokstad/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,29 +11,94 @@ namespace Console_App_Introduksjon.Gokstad
 
     /*
       * Person
-              - navn (string)
-              - fødselsår (int)
-              - ???
-              - ???
-              - Alder ??
-              - 
+              - FirstName (string)
+              - LastName
+              - FullName (sammensatt property)
+          
+              
+              - DayOfBirth (dato) -> DateTime              
+                    1: metode (int year, int month, int day)
+                    2: metode ( string -> 'yyyy-mm--dd' )
+
+              - GetAge -> kalkulere (property eller method)
+                    1: metode -> lage logikk, skrive litt kode
+            
+              - ToString() -> returnere data state fra klassen!!
+
+              // List<>()
+              - venner ??              
      */
     public class Person
     {
         // konstruktør
+        // ()
         public Person() { }
 
-        public Person(string name, int birthYear)
+        // (string, string)
+        public Person(string firstName, string lastName)
         {
-            // this -> "dette objektet"
-            Name = name;
-            BirthYear = birthYear;
+            FirstName = firstName;
+            LastName = lastName;
         }
 
-        // Properties
-        public string? Name { get; set; }
+        public Person(string firstName, string lastName, int year, int month, int day)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            DayOfBirth = new DateTime(year, month, day);
+        }
 
-        public int BirthYear { get; set; }
+
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        // 2 måter på FullName
+        // 1: property (kun lesing - ikke mulig å legge til fullname property)
+        public string? FullName 
+        {
+            get 
+            {
+                // legge inn min kode i get!!
+                return $"{FirstName} {LastName}";                
+            }
+        }
+
+        public DateTime DayOfBirth { get; set; }
+
+
+        // Methods -> Navn (lese metode "Get") -> GetFullName()
+        // yngve. må være public
+        // public <datatype ut av funksjon> <funksjonsnavn>( evt. parameter )
+        public string GetFullName()
+        {
+            //Console.WriteLine($"{FirstName} {LastName}");
+            return $"{FirstName} {LastName}";
+        }
+
+        // overloading -> ser på signaturen
+        public void SetDayOfBirth(int year, int month, int day)
+        {
+            DayOfBirth = new DateTime(year, month, day);
+        }
+
+        public void SetDayOfBirth(string date, string format)
+        {
+            CultureInfo cultureInfo = new CultureInfo("nb-NO");
+            DayOfBirth = DateTime.ParseExact(date, format, cultureInfo);
+        }
+
+        // Alder (antall år)
+        public int GetAgeInYear()
+        {
+            // tidsrom = dato nå - fødselsdato
+            TimeSpan span = DateTime.Now - DayOfBirth;
+            return (int)span.TotalDays / 365;
+        }
+
+        public override string ToString()
+        {
+            return $"FirstName={FirstName}, LastName={LastName}, FullName={FullName}, DayOfBirth={DayOfBirth.ToString()}";
+        }
     }
 
 }
--- Console App Introduksjon/Program.cs
+++ Console App Introduksjon/Program.cs
@@ -25,18 +25,122 @@ internal class Program
     private static void Main(string[] args)
     {
 
+        // StreamReader
+        // (StreamWriter)
+
+        /*
+            with open("file.txt", "r") as fin:
+                line = fin.readline()         
+                
+         */
+        List<Person> persons = new List<Person>();
+
+        string filename = "C:\\ga\\Emne 4 OOP Introduksjon\\Console App Introduksjon\\names.txt";
+        using (StreamReader reader = new StreamReader(filename))
+        {
+            string? line = reader.ReadLine();
+            while ( line != null) 
+            {
+                Person person = new Person();
+
+                // Yngve, Magnussen, 1973, 04, 30
+                // arr[0] = Yngve
+                // arr[1] = Magnussen
+                // arr[2] = 1973
+                // arr[3] = 4
+                // arr[4] = 30
+                var arr = line.Split(",");
+
+                person.FirstName = arr[0];
+                person.LastName = arr[1];
+
+
+                // legger til i listen
+                persons.Add(person);
+
+                line = reader.ReadLine();
+            }
+
+
+            foreach(Person person in persons) 
+            {
+                Console.WriteLine(person.ToString());
+            }
+            
+        }
+
+    }
+
+    private void OldStuff()
+    {
+        // ALT ER Objekt !!!!
         // Person()
-        Person yngve = new Person();
-        yngve.Name = "Yngve Magnussen";
-        yngve.BirthYear = 1973;
+        Person p1 = new Person("Yngve", "Magnussen", 1973, 04, 30);
+        Person p2 = new Person("Ola", "Normann", 2000, 10, 4);
+        Person p3 = new Person("Kari", "Normann", 1998, 7, 15);
+
+        // to måter å håndtere flere objekter
+        // 1. Array
+        // 2. List
+
+        // Array på 3 elementer!
+        Person[] persons = { p1, p2, p3 }; // bruker også new - men implisitt
+
+        /*
+
+        // for-løkke
+        // range(start, stop, step)
+        for (int i=0; i<persons.Length;i++) 
+            Console.WriteLine(persons[i].ToString());
+        
+
+        foreach (var person in persons)
+        {
+            Console.WriteLine(person.ToString());
+        }
+
+        // 1 person til !! -> så er det vanskelig
+        
+       
+        
+            Person[] persons_2 = new Person[] { p1, p2, p3 };
+
+            Person[] persons_3 = new Person[3];
+            persons_3[0] = p1;
+            persons_3[1] = p2;
+            persons_3[2] = p3;     
+        */
+
+        //yngve.SetDayOfBirth("1973-04-30", "yyyy-mm-dd");
+
+        // 2. List (generic)
+        //List<Person> people = new List<Person>()
+        List<Person> people = new();
+        people.Add(p1);
+        people.Add(p2);
+        people.Add(p3);
+
+        //for (int i = 0; i < people.Count; i++)
+        //Console.WriteLine(people[i].ToString());
+
+        /*
+        foreach (Person person in people)
+        {
+            Console.WriteLine(person.ToString());
+        }
+        */
+
+        people.ForEach(person => Console.WriteLine(person.ToString()));
+
 
 
         // Person(string name, int birthYear)
-        Person ola = new ("Ola Normann", 2000);
+        //Person ola = new Person("Ola Normann", 2000); // "" -> string, 2000 -> int
 
 
-        Console.WriteLine($"Navnet på person er {yngve.Name} og er født i år {yngve.BirthYear}");
-        Console.WriteLine($"Navnet på person er {ola.Name} og er født i år {ola.BirthYear}");
+        //Console.WriteLine($"Navnet på person er {yngve.FullName} og er født i år {yngve.DayOfBirth} Alder={yngve.GetAgeInYear()}");
+        //Console.WriteLine($"Navnet på person er {ola.Name} og er født i år {ola.BirthYear}");
 
+        //Console.WriteLine(yngve.ToString());
     }
 }
