29a1501bb8ca45e85d711dde581fc03fe7c41158
Yngve Magnussen <74648054+yngvemag@users.noreply.github.com>
2023-02-16 08:48:23 +0100

    added sql statements used to create the files

---
+++ SelectData.sql
@@ -0,0 +1,23 @@
+﻿SELECT customerNumber 'CustomerNumber',
+		customerName 'CustomerName', #,replace(customerName,',','') 'CustomerName',
+        phone 'Phone',
+        addressline1 'Address ',#replace(addressLine1,',','') 'Address', 
+        city 'City',
+        country 'Country'
+FROM classicmodels.customers;
+
+
+SELECT 	c.customerName, 
+		p.*
+FROM classicmodels.payments p
+inner join classicmodels.customers c on c.customerNumber = p.customerNumber
+order by amount desc;
+
+
+
+SELECT employeeNumber,
+		firstName,
+        lastName,
+        email,
+        jobTitle
+FROM classicmodels.employees;
